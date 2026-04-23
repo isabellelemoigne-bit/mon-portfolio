e8a71d9bbf5dd23861bd8cb8c62ce625e15db442
isabelle le moigne <276596005+isabellelemoigne-bit@users.noreply.github.com>
2026-04-23 08:54:46 +0100

    feat: selectionner les element

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// On sélectionne les éléments qu'on va manipuler
+// C'est toujours la première chose à faire en JS
+const navbar = document.getElementById("navbar");
+const burger = document.getElementById("burger");
+const navLinks = document.getElementById("nav-links");
+const backToTop = document.getElementById("back-to-top");
+const contactForm = document.getElementById("contact-form");
+const skillBars = document.querySelectorAll(".skill-progress");
