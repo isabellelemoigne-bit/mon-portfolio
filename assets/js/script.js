29a6402010c493e6bcf80e4dd98b39733dc85386
isabelle le moigne <276596005+isabellelemoigne-bit@users.noreply.github.com>
2026-04-23 09:06:44 +0100

    feat: menu burger mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,18 @@ const navLinks = document.getElementById("nav-links");
 const backToTop = document.getElementById("back-to-top");
 const contactForm = document.getElementById("contact-form");
 const skillBars = document.querySelectorAll(".skill-progress");
+// Au clic sur le burger : ouvrir/fermer le menu
+if (burger) {
+  burger.addEventListener("click", () => {
+    burger.classList.toggle("open"); // anime le X
+    navLinks.classList.toggle("open"); // fait glisser le menu
+  });
+}
+
+// Fermer le menu quand on clique un lien
+document.querySelectorAll(".nav-links a").forEach((link) => {
+  link.addEventListener("click", () => {
+    burger.classList.remove("open");
+    navLinks.classList.remove("open");
+  });
+});
